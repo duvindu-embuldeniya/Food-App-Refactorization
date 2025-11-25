77c98f8bf0f84893df7119d88498076add788aeb
duvindu-embuldeniya <duvindu.personal@gmail.com>
2025-11-25 12:13:02 +0530

    setup static and media configerations

--- app/app/urls.py
+++ app/app/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
@@ -23,3 +28,7 @@ urlpatterns = [
     path('user/', include('user.urls')),
     path('food/', include('recipe.urls')),
 ]
+
+
+
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT,)
